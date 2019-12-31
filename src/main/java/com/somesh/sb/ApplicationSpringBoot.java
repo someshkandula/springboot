9a34585b41809821bcc2ed81d40cf7b955478517
ksomalin <ksomalin@cisco.com>
2019-12-31 13:11:16 -0800

    Simple SpringBoot Application Ping Controller
    Signed-off-by: ksomalin <somesh.kandula@gmail.com>

--- src/main/java/com/somesh/sb/ApplicationSpringBoot.java
+++ src/main/java/com/somesh/sb/ApplicationSpringBoot.java
@@ -3,6 +3,11 @@ package com.somesh.sb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author ksomalin
+ *
+ */
 @SpringBootApplication
 public class ApplicationSpringBoot {
 	public static void main(String[] args) {
--- src/main/java/com/somesh/sb/controller/PingController.java
+++ src/main/java/com/somesh/sb/controller/PingController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 /**
  * 
  * @author ksomalin
@@ -15,7 +16,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("ping")
 public class PingController { 
-
     /**
      * Get ping Response 'SUCCESS' if this service is active.
      */
