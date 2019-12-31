9e4774b454fd6c2a6e6e5117fa0b99d17c90aabb
ksomalin <ksomalin@cisco.com>
2019-12-31 13:09:12 -0800

    Simple spring boot application
    Signed-off-by: Somesh <somesh.kandula@gmail.com>

---
+++ src/main/java/com/somesh/sb/controller/PingController.java
@@ -0,0 +1,27 @@
+package com.somesh.sb.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 
+ * @author ksomalin
+ *
+ */
+@Controller
+@RequestMapping("ping")
+public class PingController { 
+
+    /**
+     * Get ping Response 'SUCCESS' if this service is active.
+     */
+    @RequestMapping( method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<String> ping() {
+        return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
+    }
+}
